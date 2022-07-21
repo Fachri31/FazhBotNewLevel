28a0ed22190d54e35eaa569db5a596a0a0d2f62b
fachri <108580447+Fachri31@users.noreply.github.com>
2022-07-21 18:38:35 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,12 +2,12 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;Adul Alhy;;;'
-                    + 'FN:Adul Alhy\n' // full name
-                    + 'ORG:Adul Alhy;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6281390199407:+62 813-9019-9407\n' // WhatsApp ID + phone number
+                    + 'N:;Fachri;;;'
+                    + 'FN:Fachri??\n' // full name
+                    + 'ORG:FachriOfc;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6281390199407:+62 882-2775-8096\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'Adul Alhy', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Fachri', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
